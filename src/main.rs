d3449a246babd6d5ea073988dc77844ccf2fd329
0918nobita <nobita.0918@gmail.com>
2021-12-01 22:57:31 +0900

    :tada: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+async fn print_hello() {
+    println!("Hello, world!");
+}
+
+fn main() -> std::io::Result<()> {
+    let rt = tokio::runtime::Runtime::new()?;
+    rt.block_on(print_hello());
+    Ok(())
+}
+
+/*
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
+*/
